f6996decb3b9524116c1a1ebdcf91dd515350938
Jan Haans <jan.haans@gmail.com>
2021-06-01 17:04:34 +0200

    Added timer

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -8,33 +8,54 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvPtr := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	limitPtr := flag.Int("limit", 30, "the time limit for the quiz in seconds (defaul 30)")
 	flag.Parse()
+	end := make(chan bool)
 
-	numQuestions, numAnswers := play(*csvPtr)
-	fmt.Printf("Total questions = %d\n", numQuestions)
-	fmt.Printf("Correct answers = %d\n", numAnswers)
+	records := read(*csvPtr)
+
+	numQuestions := len(records)
+	score := 0
+
+	go play(records, &score, *limitPtr, end)
+	if <-end {
+		fmt.Printf("\nTotal questions = %d\n", numQuestions)
+		fmt.Printf("Correct answers = %d\n", score)
+	}
 
 }
 
-func play(csvFileName string) (numQuestions, numAnswers int) {
-	f, err := os.Open(csvFileName)
+func read(file string) [][]string {
+	f, err := os.Open(file)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	defer f.Close()
-
-	input := bufio.NewReader(os.Stdin)
 
 	r := csv.NewReader(f)
 	records, err := r.ReadAll()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	numQuestions = len(records)
+
+	return records
+}
+
+func play(records [][]string, score *int, limit int, c chan bool) {
+	input := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Press Enter to start the quiz")
+	_, err := input.ReadString('\n')
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	go timer(c, limit)
 
 	for i, record := range records {
 		question := record[0]
@@ -45,9 +66,14 @@ func play(csvFileName string) (numQuestions, numAnswers int) {
 			log.Fatalln(err)
 		}
 		if strings.TrimSpace(playerAnswer) == answer {
-			numAnswers += 1
+			*score += 1
 		}
 	}
 
-	return numQuestions, numAnswers
+	c <- true
+}
+
+func timer(c chan bool, limit int) {
+	time.Sleep(time.Duration(limit) * time.Second)
+	c <- true
 }
